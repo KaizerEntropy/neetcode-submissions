21f81fcd70e324389c60702f5c9e88043b101b42
Diptarko Bhattacharjee <diptarko.1312@gmail.com>
2026-06-21 20:53:20 +0530

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.cpp
@@ -0,0 +1,14 @@
+#include <cstring>
+class Solution {
+public:
+int dp[102];
+int f(int i, vector<int> &nums){
+    if(dp[i]!=-1) return dp[i];
+    if(i>=nums.size()) return dp[i]=0;
+    return dp[i] = max(f(i+1, nums), f(i+2, nums)+nums[i]);
+}
+    int rob(vector<int>& nums) {
+        memset(dp, -1, sizeof(dp));
+        return f(0, nums);
+    }
+};
